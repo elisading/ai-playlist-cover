76d873828c9c50a3318959b5972f3a85c7665d83
elisading <elisa.ding@hotmail.com>
2024-07-22 19:30:44 -0400

    compress image for upload

--- spotify/utils.py
+++ spotify/utils.py
@@ -2,6 +2,8 @@ import requests
 import base64
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
+from PIL import Image
+import io
 
 urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -34,7 +36,8 @@ def convert_image_to_base64(image_url):
     
     if response.status_code == 200:
         print("Image successfully fetched from URL.")
-        image_data = base64.b64encode(response.content).decode('utf-8')
+        compressed_image = compress_image(response.content)
+        image_data = base64.b64encode(compressed_image).decode('utf-8')
         print("Image successfully converted to base64.")
         return image_data
     else:
@@ -46,7 +49,21 @@ def upload_to_spotify(playlist_id, image_base64, access_token):
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "image/jpeg"
     }
-    response = requests.put(f"{API_BASE_URL}/playlists/{playlist_id}/images", headers=headers, data=image_base64.encode('utf-8'))
+    response = requests.put(f"{API_BASE_URL}/playlists/{playlist_id}/images", headers=headers, data=image_base64.encode('utf-8'), verify=False)
     print(f"Response Code: {response.status_code}")
     print(f"Response Content: {response.text}")
     return response.json()
+
+def compress_image(image_data, max_size_kb=256):
+    with Image.open(io.BytesIO(image_data)) as img:
+        img_format = img.format
+        output = io.BytesIO()
+        quality = 85
+        img.save(output, format=img_format, quality=quality)
+        
+        while output.tell() / 1024 > max_size_kb and quality > 10:
+            output = io.BytesIO()
+            quality -= 5
+            img.save(output, format=img_format, quality=quality)
+        
+        return output.getvalue()
